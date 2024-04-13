aea789e16a9322aca87690fd4a383937bb712091
Tomek <tomasz.chajdas@gmail.com>
2024-04-13 10:17:34 +0200

    Playing with tailwind

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Hello Tailwind World!</h1>
+    <div className="bg-neutral-200 h-screen flex justify-center items-center">
+      <div className="flex h-[600px] w-[1000px] bg-stone-500 p-1 space-x-1">
+        <div className="flex flex-col w-1/3 space-y-1">
+          <div className="bg-red-300 rounded h-1/2 w-full"></div>
+          <div className="bg-red-400 rounded h-1/2 w-full"></div>
+        </div>
+        <div className="bg-red-600 rounded flex-1 w-2/3"></div>
+      </div>
 
-    </div>
+
+    </div >
   );
 }
 

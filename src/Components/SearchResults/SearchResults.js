42f273142486f7248fecc52177e5bc4344ae3d3d
Tomek <tomasz.chajdas@gmail.com>
2024-04-19 17:19:54 +0200

    Change background color of SearchResults

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -2,7 +2,7 @@ const SearchResults = (props) => {
 
 
     return (
-        <div className="bg-black opacity-70 w-96 rounded min-h-80 px-2 py-1">
+        <div className="bg-[rgba(0,0,0,0.7)] w-96 rounded min-h-80 px-2 py-1">
             <h1 className="text-white text-2xl">Results</h1>
 
         </div>

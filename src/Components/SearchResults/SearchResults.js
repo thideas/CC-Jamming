12aa267426ae38489e6258d1ddcaefe19218d799
Tomek <tomasz.chajdas@gmail.com>
2024-04-19 17:38:17 +0200

    Add dummy track component

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,9 +1,13 @@
+import Track from "../Track/Track";
+
 const SearchResults = (props) => {
 
 
     return (
         <div className="bg-[rgba(0,0,0,0.7)] w-96 rounded min-h-80 px-2 py-1">
             <h1 className="text-white text-2xl">Results</h1>
+            <Track />
+            <Track />
 
         </div>
     )
---
+++ src/Components/Track/Track.js
@@ -0,0 +1,16 @@
+const Track = () => {
+
+
+    return (
+        <div className="py-2 border-b-[1px] border-[rgba(255,255,255,0.5)] flex text-white justify-between">
+            <div className="">
+                <div className="w-full">SongName</div>
+                <div className="text-sm text-gray-300">Song Artist</div>
+            </div>
+            <button className="text-center text-2xl px-2">+</button>
+        </div>
+    )
+
+}
+
+export default Track;

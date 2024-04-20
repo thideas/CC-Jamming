aae6847649d569bb49f5c24cd68fe9c7b33002da
Tomek <tomasz.chajdas@gmail.com>
2024-04-20 20:31:49 +0200

    Update search button and font weight

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -5,8 +5,8 @@ const SearchBar = () => {
 
     return (
         <div className="py-12 flex flex-col items-center justify-center">
-            <input className="w-48 px-2 py-1 rounded-lg" placeholder="Search for songs..." />
-            <button className="py-1 px-8 my-3 bg-green-500 rounded-full text-white">Submit</button>
+            <input className="w-52 px-2 py-1 rounded-lg" placeholder="Search for songs..." />
+            <button className="py-1 px-8 my-3 bg-green-500 rounded-full text-white">Search</button>
 
         </div>
     )

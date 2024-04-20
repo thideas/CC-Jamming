c07094e49523566dc7628b46773980efef427fca
Tomek <tomasz.chajdas@gmail.com>
2024-04-20 21:06:44 +0200

    Fix Track display

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -2,8 +2,8 @@ const Track = () => {
 
 
     return (
-        <div className="py-2 border-b-[1px] border-[rgba(255,255,255,0.5)] flex text-white justify-between">
-            <div className="">
+        <div className="py-2 border-b-[1px] border-[rgba(255,255,255,0.5)] flex text-white">
+            <div className="w-full">
                 <div className="w-full">SongName</div>
                 <div className="text-sm text-gray-300">Song Artist</div>
             </div>

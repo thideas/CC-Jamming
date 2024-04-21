7c58bce3727045aca07c67a2c5e972569351c640
Tomek <tomasz.chajdas@gmail.com>
2024-04-21 13:29:10 +0200

    Fix flex container for search and playlist to items-start

--- src/App.js
+++ src/App.js
@@ -44,12 +44,12 @@ function App() {
 
 
   return (
-    <div className="p-1 bg-[linear-gradient(to_right_bottom,rgba(0,0,0,0.4),rgba(0,0,0,0.4)),url('./img/background.webp')] h-screen w-screen bg-cover bg-center">
+    <div className="p-1 bg-[linear-gradient(to_right_bottom,rgba(0,0,0,0.4),rgba(0,0,0,0.4)),url('./img/background.webp')] h-screen w-screen bg-cover bg-center overflow-scroll">
       <div className="rounded bg-black w-full h-16 py-4 opacity-70">
         <h1 className="text-neutral-50 text-xl text-center">Jamming App</h1>
       </div>
       <SearchBar />
-      <div className="py-1 flex flex-wrap justify-center">
+      <div className="py-1 flex flex-wrap justify-center items-start">
         <SearchResults songList={searchResult} addSongToPlayList={addSongToPlaylist} />
         <Playlist playList={playList} />
 

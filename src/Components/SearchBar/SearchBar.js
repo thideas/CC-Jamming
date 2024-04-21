9a56516e107d14c90215449c01ac6cc474b110e3
Tomek <tomasz.chajdas@gmail.com>
2024-04-21 18:34:07 +0200

    Add submit on enter to searchbar

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -9,13 +9,19 @@ const SearchBar = (props) => {
         setSearchTerm(e.target.value)
     }
 
+    const handleKeyPress = (e) => {
+        if (e.key === 'Enter') {
+            props.searchSpotify(searchTerm)
+        }
+    }
+
 
 
 
     return (
         <div className="py-12 flex flex-col items-center justify-center">
-            <input className="w-52 px-2 py-1 rounded-lg focus:outline-none" placeholder="Search for songs..." value={searchTerm} onChange={handleChange} />
-            <button className="py-1 px-8 my-3 bg-green-500 rounded-full text-white" onClick={() => { props.searchSpotify(searchTerm) }}>Search</button>
+            <input className="w-52 px-2 py-1 rounded-lg focus:outline-none" placeholder="Search for songs..." value={searchTerm} onChange={handleChange} onKeyDown={handleKeyPress} />
+            <button className="py-1 px-8 my-3 bg-green-500 rounded-full text-white" onClick={() => { props.searchSpotify(searchTerm) }} >Search</button>
 
         </div>
     )

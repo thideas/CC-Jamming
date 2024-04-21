3c75e41d27808a1c41158a6f9ca7a8409d64fdb8
Tomek <tomasz.chajdas@gmail.com>
2024-04-21 17:55:19 +0200

    Add useState and handleChange to search bar

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,11 +1,20 @@
+import { useState } from "react";
+
 
 const SearchBar = (props) => {
+
+    const [searchTerm, setSearchTerm] = useState("");
+
+    const handleChange = (e) => {
+        setSearchTerm(e.target.value)
+    }
+
 
 
 
     return (
         <div className="py-12 flex flex-col items-center justify-center">
-            <input className="w-52 px-2 py-1 rounded-lg" placeholder="Search for songs..." />
+            <input className="w-52 px-2 py-1 rounded-lg focus:outline-none" placeholder="Search for songs..." value={searchTerm} onChange={handleChange} />
             <button className="py-1 px-8 my-3 bg-green-500 rounded-full text-white" onClick={() => { props.searchSpotify() }}>Search</button>
 
         </div>

07cebbc3efd42e06a987735dca70e18a439e40bf
Tomek <tomasz.chajdas@gmail.com>
2024-04-26 12:36:32 +0200

    Delete alternative text for songname, artist and album in Track

--- src/Components/Track/Track.js
+++ src/Components/Track/Track.js
@@ -4,8 +4,8 @@ const Track = (props) => {
     return (
         <div className="py-2 border-b-[1px] border-[rgba(255,255,255,0.5)] flex text-white">
             <div className="w-full">
-                <div className="w-full">{props.song?.name || "Song Title"}</div>
-                <div className="text-sm text-gray-300">{props.song?.artists.map(({ name }) => name).join(", ") || "Song Artist"} | {props.song?.album.name || "Album name"}</div>
+                <div className="w-full">{props.song?.name}</div>
+                <div className="text-sm text-gray-300">{props.song?.artists.map(({ name }) => name).join(", ")} | {props.song?.album.name}</div>
             </div>
             <button className="text-center text-2xl px-2" onClick={() => props.onAddOrDelete(props.song)}>{props.sign}</button>
         </div>

b0a1036bedc261d9760ba23d818f646503e07394
Tomek <tomasz.chajdas@gmail.com>
2024-04-27 17:04:27 +0200

    Add responsive breakpoint to profile picture size

--- src/App.js
+++ src/App.js
@@ -103,7 +103,7 @@ function App() {
 
 
           <button className="py-1 pr-3  text-white absolute right-4 top-2 flex items-center" onClick={logOut}>
-            <img className="rounded-full w-10 h-10" src={userProfile?.images[0].url}></img>
+            <img className="rounded-full w-8 h-8 sm:w-10 sm:h-10" src={userProfile?.images[0].url}></img>
             <svg class="h-4 w-4 text-white mx-2" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">  <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />  <polyline points="16 17 21 12 16 7" />  <line x1="21" y1="12" x2="9" y2="12" /></svg>
             Log out
           </button>
